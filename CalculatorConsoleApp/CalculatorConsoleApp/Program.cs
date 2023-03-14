aadce6c8bb8781c3d4e54b5bec29af54fc7019c7
rifadeta <127839764+rifadeta@users.noreply.github.com>
2023-03-14 17:08:49 +0700

    Add files via upload

---
+++ CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, hasil; //untuk menyatakan bilangangan
+            string op; //untuk menyatakan karakter
+
+            Console.WriteLine("Calculator");
+
+            Console.Write("a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Pilih Operator (+, -, *, /): ");
+            op = Console.ReadLine();
+
+            Console.Write("b: ");
+            b = double.Parse(Console.ReadLine());   
+
+
+            //menggunakan if-else-if untuk memberi comand pilihan
+
+            if(op == "+")
+            {
+                hasil = a + b;
+                Console.WriteLine("= " + hasil);
+            }
+            else if(op == "-")
+            {
+                hasil = a - b;
+                Console.WriteLine("= " + hasil);
+            }
+            else if (op == "*")
+            {
+                hasil = a * b;
+                Console.WriteLine("= " + hasil);
+            }
+            else if (op == "/")
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Invalid");
+                }
+                else
+                {
+                    hasil = a / b;
+                    Console.WriteLine("= " + hasil);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.WriteLine("Tekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+    }
+}
